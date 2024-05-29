45f388f24ffdd81796db2e7c87dea5811bb249fe
ptrscll <ptrscll@gmail.com>
2024-05-29 17:47:53 -0400

    Added initial code for all questions

--- 01/1A.intro-hector-runs.R
+++ 01/1A.intro-hector-runs.R
@@ -1,6 +1,6 @@
-# Description:
-# By:
-# Date:
+# Description: Script to practice running Hector and plotting in R
+# By: Peter Scully (questions from Kalyn Dorheim)
+# Date: 5/29-5/30/24
 # 0. Set Up --------------------------------------------------------------------
 # Call packages here & set directories if needed.
 library(hector)
@@ -23,22 +23,73 @@ ssp119_out <- fetchvars(core, dates = yrs, vars = hector_vars)
 # Get results for 1850 to 2100 for global mean air temperature and [CO2], see
 # section 1 and https://jgcri.github.io/hector/articles/hector.html for examples
 
+ini <- system.file(package = "hector", "input/hector_ssp245.ini")
+core <- newcore(ini, name = "ssp245")
+run(core)
+ssp245_out <- fetchvars(core, dates = yrs, vars = hector_vars)
+
 
 # 3. Run SSP585 ----------------------------------------------------------------
 # Get results for 1850 to 2100 for global mean air temperature and [CO2]
 
+ini <- system.file(package = "hector", "input/hector_ssp585.ini")
+core <- newcore(ini, name = "ssp585")
+run(core)
+ssp585_out <- fetchvars(core, dates = yrs, vars = hector_vars)
 
 # 4. Plots ---------------------------------------------------------------------
 #  R skills used here - concatenating data, sets together, subsetting/filtering data,
 # ggplot2 data visualization
 
-# A) Three plots showing one variable at at time changing over time for the three
+# A) Three plots showing one variable at a time changing over time for the three
 # different scenarios
+
+# Combining all of the dataframes
+all_ssp_out <- rbind(ssp119_out, ssp245_out, ssp585_out)
+
+# Setting base plot aesthetics
+base <- ggplot(data = all_ssp_out, aes(x = year, y = value, color = scenario))
+
+# Creating a plot for each variable
+for (curr_var in hector_vars) {
+  
+  # Determining units
+  if (curr_var == CONCENTRATIONS_CO2()) {
+    curr_units <- "(ppmv)"
+  } else {
+    curr_units <- "(\u00B0C)"
+  }
+  
+  # Making a plot using only values for the variable currently being plotted
+  # Also updates the y-axis and plot titles accordingly
+  curr_plot <- base + 
+    geom_line(data = filter(all_ssp_out, variable == curr_var)) +
+    labs(y = paste(curr_var, curr_units), 
+         title = paste(curr_var, "from 1850-2100"))
+  ggsave(paste(curr_var, "time.png", sep = "_"), curr_plot)
+}
+
 
 # B) A single plot of the results (will want to facet plot)
 
+# Making a plot with 3 panels (one for each variable)
+plot_B <- base + 
+  geom_line() + 
+  facet_wrap(~ variable, scales = "free") +
+  ggtitle("Overall Results")
+ggsave("combined_plot.png")
+
 # C) Plot comparing the two different temperatures with one another
 
+# Making plot with just temperature variables
+# Temperatures are distinguished by their line type
+# Scenarios are still distinguished by color
+plot_C <- base + 
+  geom_line(data = filter(all_ssp_out, 
+                          variable == GLOBAL_TAS() | variable == GMST()),
+            aes(linetype = variable)) +
+  labs(y = "Temperature (\u00B0C)", title = "Temperature Comparison")
+ggsave("T_comparison.png")
 
 
 
